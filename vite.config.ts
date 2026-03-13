1471d739dbc7bf16b4aa7df59836dffc9c12c2c4
navelorange1999 <qichen1999@qq.com>
2026-03-13 10:14:21 +0800

    Configure static deployment for Cloudflare and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-import pkg from "./package.json";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV !== "development"
-      ? `https://${pkg.author}.github.io/${pkg.name}/`
-      : "/",
+  base: process.env.BASE_URL || "/",
   plugins: [vue()],
 });
